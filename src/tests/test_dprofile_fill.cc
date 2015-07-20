898a13ca6230560de4ab02ab5f445d74ec1c0661
ronnysoak <ronny.soak@gmail.com>
2015-07-20 17:39:58 +0200

    added test for dprofile fill

--- src/tests/test_dprofile_fill.cc
+++ src/tests/test_dprofile_fill.cc
@@ -19,90 +19,141 @@
 
 #include "tests.h"
 
+#include <string.h>
+
 #include "../vsearch.h"
 #include "../align_simd.h"
 #include "../util.h"
 
 #define CDEPTH 4
 #define CHANNELS 8
-
-static struct s16info_s * s16;
+#define SCORE_MATRIX_DIM 16
 
 extern void dprofile_fill16(CELL * dprofile_word,
-        CELL * score_matrix_word,
-        BYTE * dseq);
+                            CELL * score_matrix_word,
+                            BYTE * dseq);
 
 /* Is run once before each unit test */
-static void setup() {
-	CELL match = 5;
-	CELL mismatch = -4;
-	CELL gap_open = 2;
-	CELL gap_extension = 3;
-
-	s16 = search16_init(match, mismatch,
-			gap_open, gap_open, gap_open, gap_open, gap_open, gap_open,
-			gap_extension, gap_extension, gap_extension, gap_extension, gap_extension, gap_extension);
+static void setup()
+{
+  opt_match = 5;
+  opt_mismatch = -4;
 }
 
 /* Is run once after each unit test */
-static void teardown() {
+static void teardown()
+{
+}
+
+void print_profile(CELL * dprofile)
+  {
+    for (int i = 0; i<SCORE_MATRIX_DIM; ++i)
+      {
+        for (int j = 0; j<CDEPTH; ++j)
+          {
+            for (int k = 0; k<CHANNELS; k++)
+              {
+                printf("%2d ", dprofile[CHANNELS*CDEPTH*i+CHANNELS*j+k]);
+              }
+            printf(" | ");
+          }
+        printf("\n");
+      }
+  }
+
+void print_matrix(CELL matrix[SCORE_MATRIX_DIM*SCORE_MATRIX_DIM])
+{
+  // end copy
+  for (int i = 0; i<SCORE_MATRIX_DIM; i++)
+    {
+      for (int j = 0; j<SCORE_MATRIX_DIM; j++)
+        {
+          printf("%2d, ", matrix[SCORE_MATRIX_DIM*i+j]);
+        }
+      printf("\n");
+    }
+  printf("\n");
+}
+
+void print_search_window(BYTE * dseq)
+{
+  for (int i = 0; i<CDEPTH; i++)
+    {
+      for (int j = 0; j<CHANNELS; j++)
+        {
+          printf("%2d ", dseq[CHANNELS*i+j]);
+        }
+      printf("\n");
+    }
+}
+
+void check_profile(CELL matrix[SCORE_MATRIX_DIM*SCORE_MATRIX_DIM], CELL* dprofile, BYTE* dseq)
+{
+  for (int i = 0; i<SCORE_MATRIX_DIM; ++i)
+    {
+      for (int j = 0; j<CDEPTH; ++j)
+        {
+          for (int k = 0; k<CHANNELS; k++)
+            {
+              CELL pval = dprofile[CHANNELS*CDEPTH*i+CHANNELS*j+k];
+              CELL dval = dseq[CHANNELS*j+k];
+
+              ck_assert_int_eq(matrix[SCORE_MATRIX_DIM*dval+i], pval);
+            }
+        }
+    }
+}
+
+/* copied from align_simd.c -> search16_init() */
+void fill_matrix(CELL matrix[SCORE_MATRIX_DIM*SCORE_MATRIX_DIM])
+{
+  for (int i = 0; i<SCORE_MATRIX_DIM; i++)
+    for (int j = 0; j<SCORE_MATRIX_DIM; j++)
+      {
+        CELL value;
+        if (i==j)
+          value = opt_match;
+        else if ((i==0)||(j==0)||(i>4)||(j>4))
+          value = 0;
+        else
+          value = opt_mismatch;
+        matrix[SCORE_MATRIX_DIM*i+j] = value;
+      }
 }
 
 START_TEST (test_dprofile_fill_nucleotide_simple)
-	{
+    {
+      BYTE * dseq = (BYTE *)calloc(1, CDEPTH * CHANNELS);
 
-	BYTE * d_begin[CHANNELS];
-	BYTE * d_end[CHANNELS];
+      int k = 1;
 
-	for (int c=0; c<CHANNELS; c++)
-	{
-	  d_begin[c] = 0;
-	  d_end[c] = d_begin[c];
-	}
+      int dseq_orig[][4] = {{ 'A', 'C', 'A', 'T' }, }; // ACAT
+      for (int j = 0; j < k; ++j)
+          for (int i = 0; i < CDEPTH; ++i) {
+              dseq[CHANNELS*i+k] = chrmap_4bit[dseq_orig[j][i]];
+          }
+//      print_search_window(dseq);
 
-	char * dseq_orig = (char*) xmalloc(4);
-	strcpy(dseq_orig, "ACAT");
+      CELL matrix[SCORE_MATRIX_DIM*SCORE_MATRIX_DIM];
 
-	d_begin[0] = (BYTE*) dseq_orig;
+      fill_matrix(matrix);
+//      print_matrix(matrix);
 
-	__m128i dseqalloc[CDEPTH];
-	BYTE * dseq = (BYTE*) &dseqalloc;
+      CELL * dprofile = (CELL*)xmalloc(2*4*8*16);
 
-	for (int c = 0; c < CHANNELS; c++) {
-		for (int j = 0; j < CDEPTH; j++) {
-			if (d_begin[c] < d_end[c])
-				dseq[CHANNELS * j + c] = chrmap_4bit[*(d_begin[c]++)];
-			else
-				dseq[CHANNELS * j + c] = 0;
-		}
-	}
+      dprofile_fill16(dprofile, matrix, dseq);
+//      print_profile(dprofile);
 
-	__m128i matrix[32];
+      check_profile(matrix, dprofile, dseq);
+    }END_TEST
 
-	for (int i = 0; i < 16; i++)
-		for (int j = 0; j < 16; j++) {
-			CELL value;
-			if (i == j)
-				value = opt_match;
-			else if ((i == 0) || (j == 0) || (i > 4) || (j > 4))
-				value = 0;
-			else
-				value = opt_mismatch;
-			((CELL*) (matrix))[16 * i + j] = value;
-		}
+void add_dprofile_fill_TC(Suite *s)
+{
+  TCase *tc_core = tcase_create("dprofile fill");
 
-	__m128i * dprofile = (__m128i *) xmalloc(2*4*8*16);
+  tcase_add_checked_fixture(tc_core, &setup, &teardown);
 
-	dprofile_fill16((CELL*) dprofile, (CELL*) matrix, dseq);
+  tcase_add_test(tc_core, test_dprofile_fill_nucleotide_simple);
 
-	}END_TEST
-
-void add_dprofile_fill_TC( Suite *s ) {
-    TCase *tc_core = tcase_create( "dprofile fill" );
-
-    tcase_add_checked_fixture(tc_core, &setup, &teardown);
-
-    tcase_add_test(tc_core, test_dprofile_fill_nucleotide_simple);
-
-    suite_add_tcase(s, tc_core);
+  suite_add_tcase(s, tc_core);
 }
