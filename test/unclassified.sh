1cff1a11b39b7252262a7e702b1373a88e8042c1
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-09-28 11:10:48 +0200

    fastx_filter reports sizein when relabeling fasta
    The relabeled fasta entry receives an abundance of 1, instead of its
    original abundance value (see issue #204).

--- test/unclassified.sh
+++ test/unclassified.sh
@@ -185,7 +185,7 @@ DESCRIPTION="entries shorter than the --fastq_trunclength value are discarded"
     --quiet \
     --fastqout - \
     2> /dev/null | \
-    grep "seq1" && \
+    grep -q "seq1" && \
     failure "${DESCRIPTION}" || \
         success  "${DESCRIPTION}"
 
@@ -196,8 +196,33 @@ DESCRIPTION="entries equal or longer than the --fastq_trunclength value are kept
     --quiet \
     --fastqout - \
     2> /dev/null | \
-    grep "seq1" && \
+    grep -q "seq1" && \
     success  "${DESCRIPTION}" || \
         failure "${DESCRIPTION}"
+
+
+#*****************************************************************************#
+#                                                                             #
+#     fastx_filter ignores sizein when relabeling fasta input (issue #204)    #
+#                                                                             #
+#*****************************************************************************#
+
+# https://github.com/torognes/vsearch/issues/204
+#
+# --fastx_filter ignores input sequence abundances when relabeling
+# with fasta input, --sizein and --sizeout options
+DESCRIPTION="fastx_filter reports sizein when relabeling fasta (issue #204)"
+"${VSEARCH}" \
+    --fastx_filter <(printf ">seq1;size=5;\nACGT\n") \
+    --sizein \
+    --relabel_md5 \
+    --sizeout \
+    --quiet \
+    --fastaout - \
+    2> /dev/null | \
+    grep -q ";size=5;" && \
+    success  "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
 
 exit 0
