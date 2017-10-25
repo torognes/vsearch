24915477d550eeb7ef64af76d5531ec4dd02f9c0
Torbjørn Rognes <torognes@ifi.uio.no>
2017-10-25 13:51:19 +0200

    VSEARCH 2.5.1: Fix bad minseqlength with makeudb_usearch command

--- src/vsearch.cc
+++ src/vsearch.cc
@@ -2021,7 +2021,8 @@ void args_init(int argc, char **argv)
   if (opt_minseqlength < 0)
     {
       if (opt_cluster_smallmem || opt_cluster_fast || opt_cluster_size ||
-          opt_usearch_global || opt_derep_fulllength || opt_derep_prefix )
+          opt_usearch_global || opt_derep_fulllength || opt_derep_prefix ||
+          opt_makeudb_usearch)
         opt_minseqlength = 32;
       else
         opt_minseqlength = 1;
