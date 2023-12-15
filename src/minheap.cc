415d993493fc87510a296c4399b1f696b2cfc86d
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-12-15 11:36:09 +0100

    include-cleaner

--- src/minheap.cc
+++ src/minheap.cc
@@ -59,8 +59,8 @@
 */
 
 #include "vsearch.h"
+#include <cstdio>  // printf
 #include <cstdlib>  // qsort()
-
 
 /* implement a priority queue with a min heap binary array structure */
 /* elements with the lowest count should be at the top (root) */
