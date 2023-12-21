6f855653478fda1ad25fa20cc5b5e53fd684f9d7
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-12-21 11:09:36 +0100

    fix spacing

--- src/sha1.c
+++ src/sha1.c
@@ -210,9 +210,9 @@ void SHA1_Update(SHA1_CTX* context, const uint8_t* data, const size_t len)
 
     j = (context->count[0] >> 3) & 63;
     context->count[0] += len << 3;
-    if (context->count[0] < (len << 3)) { context->count[1]++;
-
-        }
+    if (context->count[0] < (len << 3)) {
+      context->count[1]++;
+    }
     context->count[1] += (len >> 29);
     if ((j + len) > 63) {
         memcpy(&context->buffer[j], data, (i = 64-j));
