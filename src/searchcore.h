ada8e8d93aea8597403bf1c32483e88af958f61c
Torbjørn Rognes <torognes@ifi.uio.no>
2024-01-18 09:22:40 +0100

    Add braces to avoid clang++ compiler warning

--- src/searchcore.h
+++ src/searchcore.h
@@ -65,7 +65,7 @@ constexpr auto MAXDELAYED = 8U;
 
 /* Default minimum number of word matches for word lengths 3-15 */
 constexpr std::array<int, 16> minwordmatches_defaults =
-  { -1, -1, -1, 18, 17, 16, 15, 14, 12, 11, 10,  9,  8,  7,  5,  3 };
+  {{ -1, -1, -1, 18, 17, 16, 15, 14, 12, 11, 10,  9,  8,  7,  5,  3 }};
 
 struct hit
 {
