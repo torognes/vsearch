617b97865c019608e2e7228ac0bc625ad42874c0
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-04-24 14:11:51 +0200

    cppcoreguidelines-pro-type-cstyle-cast

--- src/msa.cc
+++ src/msa.cc
@@ -136,7 +136,7 @@ auto msa(std::FILE * fp_msaout, std::FILE * fp_consout, std::FILE * fp_profile,
   int const centroid_len = db_getsequencelen(centroid_seqno);
 
   /* find max insertions in front of each position in the centroid sequence */
-  int * maxi = (int *) xmalloc((centroid_len + 1) * sizeof(int));
+  int * maxi = static_cast<int *>(xmalloc((centroid_len + 1) * sizeof(int)));
   std::memset(maxi, 0, (centroid_len + 1) * sizeof(int));
 
   for(int j = 1; j < target_count; j++)
