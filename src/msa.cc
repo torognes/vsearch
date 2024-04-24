4dd68999713abb5610cfbd6ac9cf38ac86107cfc
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-04-24 14:27:42 +0200

    prefer pre-increment and separate operations for readability

--- src/msa.cc
+++ src/msa.cc
@@ -249,7 +249,8 @@ auto msa(std::FILE * fp_msaout, std::FILE * fp_consout, std::FILE * fp_profile,
               int scanlength = 0;
               std::sscanf(p, "%" PRId64 "%n", &run, &scanlength);
               p += scanlength;
-              char const op = *p++;
+              char const op = *p;
+              ++p;
 
               if (op == 'D')
                 {
