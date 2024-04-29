1457b4c05d09dae219916e7c3724089c10c15f7b
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-04-29 16:15:26 +0200

    almost always use auto

--- src/msa.cc
+++ src/msa.cc
@@ -335,8 +335,8 @@ auto compute_and_print_msa(int const target_count,
     {
       int position_in_alignment = 0;
       auto const & target = target_list_v[i];
-      int const target_seqno = target.seqno;
-      char * const target_seq = reverse_complement_target_if_need_be(target.strand, target_seqno,
+      auto const target_seqno = target.seqno;
+      auto * const target_seq = reverse_complement_target_if_need_be(target.strand, target_seqno,
                                                                      rc_buffer, db_getsequence(target_seqno));
       prof_type const target_abundance = opt_sizein ? db_getabundance(target_seqno) : 1;
 
