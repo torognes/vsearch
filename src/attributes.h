a5dc9699d9cd490c0a14a69f07ea869b96910cf4
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-05-27 11:56:26 +0200

    modernize-use-trailing-return-type

--- src/attributes.h
+++ src/attributes.h
@@ -58,11 +58,11 @@
 
 */
 
-int64_t header_get_size(char * header, int header_length);
+auto header_get_size(char * header, int header_length) -> int64_t;
 
-void header_fprint_strip(FILE * fp,
+auto header_fprint_strip(FILE * fp,
                          char * header,
                          int header_length,
                          bool strip_size,
                          bool strip_ee,
-                         bool strip_length);
+                         bool strip_length) -> void;
