torognes/vsearch

commit 65e4eb879215ed3ac837d6ea2c32ded593287afe

prefer alternative operator representations

src/vsearch.cc
@@ -5551,12 +5551,12 @@ void cmd_search_exact()
 
 void cmd_subsample()
 {
-  if ((! opt_fastaout) && (! opt_fastqout))
+  if ((not opt_fastaout) and (not opt_fastqout))
     {
       fatal("Specify output files for subsampling with --fastaout and/or --fastqout");
     }
 
-  if ((opt_sample_pct > 0) == (opt_sample_size > 0))
+  if ((opt_sample_pct > 0) and (opt_sample_size > 0))
     {
       fatal("Specify either --sample_pct or --sample_size");
     }

commit 75a689f7a94d109383f8a2316459fcadd6c8e754

fix: bug introduced earlier in 65e4eb8

src/vsearch.cc
@@ -5556,7 +5556,7 @@ void cmd_subsample()
       fatal("Specify output files for subsampling with --fastaout and/or --fastqout");
     }
 
-  if ((opt_sample_pct > 0) and (opt_sample_size > 0))
+  if ((opt_sample_pct > 0) == (opt_sample_size > 0))
     {
       fatal("Specify either --sample_pct or --sample_size");
     }