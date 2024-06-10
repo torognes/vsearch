baa0f17146e3429f734a7b590e6028ab212f7d78
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-06-10 16:38:58 +0200

    misc-include-cleaner

--- src/fastqops.cc
+++ src/fastqops.cc
@@ -59,6 +59,12 @@
 */
 
 #include "vsearch.h"
+#include <cinttypes>  // macros PRIu64 and PRId64
+#include <climits>  // LONG_MAX
+#include <cstdint>  // int64_t, uint64_t
+#include <cstdio>  // std::FILE, std::fprintf, std::fclose
+#include <cstring>  // std::memset
+
 
 auto fastq_chars() -> void
 {
