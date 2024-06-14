6138b501fdb07bb4ec18cb67913696235da560fd
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-06-14 10:30:48 +0200

    comment

--- src/filter.cc
+++ src/filter.cc
@@ -60,7 +60,7 @@
 
 #include "vsearch.h"
 #include <cinttypes>  // macros PRIu64 and PRId64
-#include <climits>  // LONG_MIN
+#include <climits>  // LONG_MIN, DBL_MAX
 #include <cstdint>  // int64_t, uint64_t
 #include <cstdio>  // std::FILE, std::fprintf, std::fclose
 #include <cstdlib>  // std::exit, EXIT_FAILURE
