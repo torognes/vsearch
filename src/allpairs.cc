6f06262b5db0825fdae60c6cd04c4f76400bd881
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-06-16 14:31:26 +0200

    cppcoreguidelines-no-malloc

--- src/allpairs.cc
+++ src/allpairs.cc
@@ -355,8 +355,8 @@ auto allpairs_thread_run(int64_t t) -> void
   std::vector<unsigned short> paligned(maxhits);
   std::vector<unsigned short> pmatches(maxhits);
   std::vector<unsigned short> pmismatches(maxhits);
-  auto * pgaps =
-    (unsigned short *) xmalloc(sizeof(unsigned short) * maxhits);
+  std::vector<unsigned short> pgaps_v(maxhits);
+  auto * pgaps = pgaps_v.data();
   char** pcigar = (char **) xmalloc(sizeof(char *) * maxhits);
 
   auto * finalhits
@@ -546,7 +546,6 @@ auto allpairs_thread_run(int64_t t) -> void
   xfree(finalhits);
 
   xfree(pcigar);
-  xfree(pgaps);
 
   search16_exit(si->s);
 
