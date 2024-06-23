ddb605e469812ceb4b77d2caee578f9d1c8cf981
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-06-23 10:44:06 +0200

    rename variable

--- src/fa2fq.cc
+++ src/fa2fq.cc
@@ -81,8 +81,8 @@ auto fasta2fastq(struct Parameters const & parameters) -> void
     }
 
   auto counter = 0;
-  std::size_t const alloc = 1024;
-  std::vector<char> quality(alloc, max_ascii_value);
+  std::size_t const initial_length = 1024;
+  std::vector<char> quality(initial_length, max_ascii_value);
 
   progress_init("Converting FASTA file to FASTQ", fasta_get_size(fp_input));
 
