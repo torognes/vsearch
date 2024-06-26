f00b6858c09fc608d576dae358a3494de65988d1
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-06-26 15:33:32 +0200

    prefer pre-increment

--- src/cut.cc
+++ src/cut.cc
@@ -129,10 +129,10 @@ auto cut_one(fastx_handle input_handle,
   int rc_start = seq_length;
   int rc_length = 0;
 
-  for (int i = 0; i < seq_length - pattern_length + 1; i++)
+  for (int i = 0; i < seq_length - pattern_length + 1; ++i)
     {
       auto match = true;
-      for (int j = 0; j < pattern_length; j++)
+      for (int j = 0; j < pattern_length; ++j)
         {
           if ((static_cast<unsigned char>(restriction.coded_pattern[j]) &
                chrmap_4bit_array[static_cast<unsigned char>(seq[i + j])]) == 0)
