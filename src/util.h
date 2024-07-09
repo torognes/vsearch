af31dc1b7b1af3f44fc07d1aea6fb82d11c3f693
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-07-09 14:55:42 +0200

    modernize-use-using

--- src/util.h
+++ src/util.h
@@ -70,11 +70,9 @@
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #endif
 
-#define SHA_DIGEST_LENGTH SHA1_DIGEST_SIZE
 
 constexpr auto md5_digest_length = 16;
 constexpr auto sha1_digest_length = 20;
-#define LEN_DIG_SHA1 SHA_DIGEST_LENGTH
 
 constexpr auto len_hex_dig_md5 = (2 * md5_digest_length) + 1;
 constexpr int len_hex_dig_sha1 = (2 * sha1_digest_length) + 1;
