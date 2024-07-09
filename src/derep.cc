b4e8164548fe8d1d7c9455569fac2ab6e905fc85
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-07-09 10:33:39 +0200

    cppcoreguidelines-avoid-magic-numbers

--- src/derep.cc
+++ src/derep.cc
@@ -188,10 +188,12 @@ auto rehash(struct bucket ** hashtableref, int64_t alloc_clusters) -> void
 
 inline auto convert_quality_to_probability(int const quality_symbol, struct Parameters const & parameters) -> double
 {
+  static constexpr auto minimal_quality_value = 2;
+  static constexpr auto maximal_probability_value = 0.75;
   int const quality_value = quality_symbol - parameters.opt_fastq_ascii;
-  if (quality_value < 2)
+  if (quality_value < minimal_quality_value)
     {
-      return 0.75;
+      return maximal_probability_value;
     }
   else
     {
