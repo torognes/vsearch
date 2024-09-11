35d9e0031a9328390837d52a6537bc4609674a99
Torbjørn Rognes <torognes@ifi.uio.no>
2024-09-11 15:49:54 +0200

    Avoid compilation errors for Windows

--- src/sortbysize.cc
+++ src/sortbysize.cc
@@ -188,9 +188,8 @@ auto output_median_abundance(std::vector<sortinfo_size_s> const & deck,
 
 auto truncate_deck(std::vector<struct sortinfo_size_s> & deck,
                    long int const n_first_sequences) -> void {
-  auto const final_size = std::min(deck.size(),
-                                   static_cast<unsigned long>(n_first_sequences));
-  deck.resize(final_size);
+  if (deck.size() > static_cast<unsigned long>(n_first_sequences))
+    deck.resize(n_first_sequences);
 }
 
 
