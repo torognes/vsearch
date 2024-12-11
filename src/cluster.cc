torognes/vsearch

commit 045a6d51d9082e590a5260b6dd92c34ab76431f9

replace loop with std algorithm

src/cluster.cc
@@ -69,6 +69,7 @@
 #include <cstring>  // std::strcpy, std::strlen
 #include <limits>
 #include <pthread.h>
+#include <utility>  // std::get
 #include <vector>
 
 
@@ -1385,23 +1386,17 @@ auto cluster(char * dbname,
       ++cluster_size[clusterno];
     }
 
-  int64_t abundance_min = LONG_MAX;
-  int64_t abundance_max = 0;
   int size_max = 0;
-  int const singletons = std::count(cluster_abundance_v.cbegin(), cluster_abundance_v.cend(), int64_t{1});
+  auto const minmax_elements = std::minmax_element(cluster_abundance_v.cbegin(),
+                                                   cluster_abundance_v.cend());
+  int64_t const abundance_min = *std::get<0>(minmax_elements);
+  int64_t const abundance_max = *std::get<1>(minmax_elements);
+  int const singletons = std::count(cluster_abundance_v.cbegin(),
+                                    cluster_abundance_v.cend(), int64_t{1});
+
 
   for (int z = 0; z < clusters; z++)
     {
-      int64_t const abundance = cluster_abundance_v[z];
-      if (abundance < abundance_min)
-        {
-          abundance_min = abundance;
-        }
-      if (abundance > abundance_max)
-        {
-          abundance_max = abundance;
-        }
-
       int const size = cluster_size[z];
       if (size > size_max)
         {

commit 49d7ff2e2a6bc61b231340d5ed918852d18fdfb9

fix: segmentation fault with empty sets of clusters
regression introduced with commit
045a6d51d9082e590a5260b6dd92c34ab76431f9 (Jun 15 21:25:02 2024 +0200)

very easy to test, but sadly not yet covered by our test-suite:

```sh
 vsearch --cluster_unoise <(printf ">s\nAA\n") --blast6out -
```

cause: std::minmax_element() and std::max_element() return
non-deferencable iterators when the searched container is empty.

src/cluster.cc
@@ -1396,13 +1396,13 @@ auto cluster(char * dbname,
 
   auto const minmax_elements = std::minmax_element(cluster_abundance_v.cbegin(),
                                                    cluster_abundance_v.cend());
-  auto const abundance_min = *std::get<0>(minmax_elements);
-  auto const abundance_max = *std::get<1>(minmax_elements);
+  auto const abundance_min = cluster_abundance_v.empty() ? 0 : *std::get<0>(minmax_elements);
+  auto const abundance_max = cluster_abundance_v.empty() ? 0 : *std::get<1>(minmax_elements);
   int const singletons = std::count(cluster_abundance_v.cbegin(),
                                     cluster_abundance_v.cend(), int64_t{1});
   auto const max_element = std::max_element(cluster_size.cbegin(),
                                             cluster_size.cend());
-  auto const size_max = *max_element;
+  auto const size_max = cluster_size.empty() ? 0 : *max_element;
 
 
   /* Sort sequences in clusters by their abundance or ordinal number */