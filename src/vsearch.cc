d7f8697465762fef08317c43c109152abe500009
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-01-12 16:35:37 +0100

    readability-implicit-bool-conversion

--- src/vsearch.cc
+++ src/vsearch.cc
@@ -413,51 +413,51 @@ auto cpu_features_detect() -> void
 auto cpu_features_show() -> void
 {
   fprintf(stderr, "CPU features:");
-  if (neon_present)
+  if (neon_present != 0)
     {
       fprintf(stderr, " neon");
     }
-  if (altivec_present)
+  if (altivec_present != 0)
     {
       fprintf(stderr, " altivec");
     }
-  if (mmx_present)
+  if (mmx_present != 0)
     {
       fprintf(stderr, " mmx");
     }
-  if (sse_present)
+  if (sse_present != 0)
     {
       fprintf(stderr, " sse");
     }
-  if (sse2_present)
+  if (sse2_present != 0)
     {
       fprintf(stderr, " sse2");
     }
-  if (sse3_present)
+  if (sse3_present != 0)
     {
       fprintf(stderr, " sse3");
     }
-  if (ssse3_present)
+  if (ssse3_present != 0)
     {
       fprintf(stderr, " ssse3");
     }
-  if (sse41_present)
+  if (sse41_present != 0)
     {
       fprintf(stderr, " sse4.1");
     }
-  if (sse42_present)
+  if (sse42_present != 0)
     {
       fprintf(stderr, " sse4.2");
     }
-  if (popcnt_present)
+  if (popcnt_present != 0)
     {
       fprintf(stderr, " popcnt");
     }
-  if (avx_present)
+  if (avx_present != 0)
     {
       fprintf(stderr, " avx");
     }
-  if (avx2_present)
+  if (avx2_present != 0)
     {
       fprintf(stderr, " avx2");
     }
