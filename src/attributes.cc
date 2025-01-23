37ffeb62d9b3ebc1a8b20f2c2733838012bd3431
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-01-23 19:19:16 +0100

    reduce nestedness

--- src/attributes.cc
+++ src/attributes.cc
@@ -152,18 +152,18 @@ auto header_get_size(char * header, int header_length) -> int64_t
                                                           &start,
                                                           &end,
                                                           false);
-  if (attribute_is_present)
+  if (not attribute_is_present) { return abundance; }
+
+  auto const number = std::atol(header + start + 5);
+  if (number > 0)
     {
-      auto const number = std::atol(header + start + 5);
-      if (number > 0)
-        {
-          abundance = number;
-        }
-      else
-        {
-          fatal("Invalid (zero) abundance annotation in FASTA file header");
-        }
+      abundance = number;
     }
+  else
+    {
+      fatal("Invalid (zero) abundance annotation in FASTA file header");
+    }
+
   return abundance;
 }
 
