5112a466839e4ac144ad05a25cd8f18e615eb063
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-01-26 17:42:53 +0100

    almost always use auto

--- src/util.h
+++ src/util.h
@@ -66,7 +66,7 @@ constexpr auto md5_digest_length = 16;
 constexpr auto sha1_digest_length = 20;
 
 constexpr auto len_hex_dig_md5 = (2 * md5_digest_length) + 1;
-constexpr int len_hex_dig_sha1 = (2 * sha1_digest_length) + 1;
+constexpr auto len_hex_dig_sha1 = (2 * sha1_digest_length) + 1;
 
 auto fatal(const char * msg) -> void;
 auto fatal(const char * format, const char * message) -> void;
