c3651cfec247372ea026082caa26fc3d1e2e7687
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-02-01 14:32:10 +0100

    comment

--- src/fastq_stats.cc
+++ src/fastq_stats.cc
@@ -65,7 +65,7 @@
 #include <cinttypes>  // macros PRIu64 and PRId64
 #include <cmath>  // std::pow
 #include <cstdint>  // int64_t, uint64_t
-#include <cstdio>  // std::FILE, std::fprintf, std::fclose, std::size_t
+#include <cstdio>  // std::fprintf, std::size_t
 #include <limits>
 #include <vector>
 
