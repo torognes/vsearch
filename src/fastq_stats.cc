45f69d0d4b76f34746d003b00385aed4f3af14b5
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-02-15 10:17:51 +0100

    comment

--- src/fastq_stats.cc
+++ src/fastq_stats.cc
@@ -96,8 +96,8 @@ auto q2p(double quality_score) -> double {
 
 // auto q2p(uint64_t quality_score) -> double {
 //   static constexpr auto base = 10.0;
-//   auto const q_score = static_cast<double>(quality_score);
-//   return std::pow(base, -q_score / base);
+//   auto const quality_score_double = static_cast<double>(quality_score);
+//   return std::pow(base, -quality_score_double / base);
 // }
 
 
