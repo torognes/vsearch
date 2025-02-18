aeed53bed0f1b45f3e1985c90a489ca202595573
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-02-18 10:51:42 +0100

    comment

--- src/fastq_stats.cc
+++ src/fastq_stats.cc
@@ -520,7 +520,7 @@ auto fastq_stats(struct Parameters const & parameters) -> void
 
       auto * quality_symbols = fastq_get_quality(input_handle);
       auto expected_error = 0.0;
-      auto qmin = std::numeric_limits<uint64_t>::max();  // lowest Q value observed in this read
+      auto qmin = std::numeric_limits<uint64_t>::max();  // lowest Q value observed so far in this read
 
       check_minmax_scores(Span{quality_symbols, length}, symbol_to_score, parameters);
 
