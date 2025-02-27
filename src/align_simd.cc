78013bfdc71344bfa03b23c049fa17dcca59c530
Torbjørn Rognes <torognes@ifi.uio.no>
2025-02-27 17:21:44 +0100

    Use optimization pragma just for GNU compiler, not clang

--- src/align_simd.cc
+++ src/align_simd.cc
@@ -1224,7 +1224,11 @@ auto search16_qprep(s16info_s * s, char * qseq, int qlen) -> void
   GNU C++ 9 or later generates incorrect code on x86_64 if turned on.
 */
 
+#ifdef __GNUC__
+#ifndef __clang__
 #pragma GCC optimize ("-fno-tree-partial-pre")
+#endif
+#endif
 
 auto search16(s16info_s * s,
               unsigned int sequences,
