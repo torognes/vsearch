0d3cf5775cdd0d2c54f11252416b72d6a881be5a
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-02-28 09:43:15 +0100

    comment

--- src/filter.cc
+++ src/filter.cc
@@ -254,7 +254,7 @@ auto analyse(fastx_handle input_handle) -> struct analysis_res
 
 auto filter(bool const fastq_only, char * filename) -> void
 {
-  static constexpr auto dbl_max = std::numeric_limits<double>::max();
+  static constexpr auto dbl_max = std::numeric_limits<double>::max();  // refactoring: redundant?
   static constexpr auto long_min = std::numeric_limits<long>::min();
 
   if ((opt_fastqout == nullptr) and (opt_fastaout == nullptr) and
