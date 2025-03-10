torognes/vsearch

commit 91d779ab91eb33184b65c4ec2ee2c7d9ebf2caab

direct initialization

src/mergepairs.cc
@@ -605,12 +605,6 @@ auto merge(merge_data_t * a_read_pair) -> void
   auto const fwd_5prime_overhang = (a_read_pair->fwd_trunc > a_read_pair->offset) ?
     a_read_pair->fwd_trunc - a_read_pair->offset : 0;
 
-  a_read_pair->ee_merged = 0.0;
-  a_read_pair->ee_fwd = 0.0;
-  a_read_pair->ee_rev = 0.0;
-  a_read_pair->fwd_errors = 0;
-  a_read_pair->rev_errors = 0;
-
   auto sym = '\0';
   auto qual = '\0';
   auto fwd_sym = '\0';

commit fd05f30a7dddf11913f87d015311a30c1e402706

fix: regression in --fastq_mergepairs --eetabbedout
- regression introduced in commit 91d779ab91eb33184b65c4ec2ee2c7d9ebf2caab
- if struct members are not properly reset, then --eetabbedout contains
  erroneous values (only visible after several hundreds of merges)

src/mergepairs.cc
@@ -605,6 +605,12 @@ auto merge(merge_data_t * a_read_pair) -> void
   auto const fwd_5prime_overhang = (a_read_pair->fwd_trunc > a_read_pair->offset) ?
     a_read_pair->fwd_trunc - a_read_pair->offset : 0;
 
+  // reset struct
+  a_read_pair->ee_merged = 0.0;
+  a_read_pair->ee_fwd = 0.0;
+  a_read_pair->ee_rev = 0.0;
+  a_read_pair->fwd_errors = 0;
+  a_read_pair->rev_errors = 0;
   auto sym = '\0';
   auto qual = '\0';
   auto fwd_sym = '\0';