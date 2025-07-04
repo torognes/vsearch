e81bf0689fb7d5a33757ed7b7f4a0fd1e2df20dc
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-04 17:26:33 +0200

    pass scoring parameters through struct

--- src/cluster.cc
+++ src/cluster.cc
@@ -653,21 +653,23 @@ auto cluster_core_parallel() -> void
   scoring.match = opt_match;
   scoring.mismatch = opt_mismatch;
   scoring.gap_open_query_interior = opt_gap_open_query_interior;
+  scoring.gap_extension_query_interior = opt_gap_extension_query_interior;
+  scoring.gap_open_query_left = opt_gap_open_query_left;
+  scoring.gap_open_target_left = opt_gap_open_target_left;
+  scoring.gap_open_query_interior = opt_gap_open_query_interior;
+  scoring.gap_open_target_interior = opt_gap_open_target_interior;
+  scoring.gap_open_query_right = opt_gap_open_query_right;
+  scoring.gap_open_target_right = opt_gap_open_target_right;
+  scoring.gap_extension_query_left = opt_gap_extension_query_left;
+  scoring.gap_extension_target_left = opt_gap_extension_target_left;
+  scoring.gap_extension_query_interior = opt_gap_extension_query_interior;
+  scoring.gap_extension_target_interior = opt_gap_extension_target_interior;
+  scoring.gap_extension_query_right = opt_gap_extension_query_right;
+  scoring.gap_extension_target_right = opt_gap_extension_target_right;
+
 
   LinearMemoryAligner lma(scoring);
 
-  lma.set_parameters(opt_gap_open_query_left,
-                     opt_gap_open_target_left,
-                     opt_gap_open_query_interior,
-                     opt_gap_open_target_interior,
-                     opt_gap_open_query_right,
-                     opt_gap_open_target_right,
-                     opt_gap_extension_query_left,
-                     opt_gap_extension_target_left,
-                     opt_gap_extension_query_interior,
-                     opt_gap_extension_target_interior,
-                     opt_gap_extension_query_right,
-                     opt_gap_extension_target_right);
 
   auto lastlength = std::numeric_limits<int>::max();
 
