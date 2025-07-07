c35b801c51cdfabba03c38e780c021f133fb9626
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-07 11:17:08 +0200

    eliminate direct call to malloc/free

--- src/derep.cc
+++ src/derep.cc
@@ -141,7 +141,7 @@ auto derep_compare_full(void const * void_lhs, void const * void_rhs) -> int
 }
 
 
-auto rehash(struct bucket ** hashtableref, int64_t alloc_clusters) -> void
+auto rehash(std::vector<struct bucket> & hashtable_v, int64_t alloc_clusters) -> void
 {
   /*
     double the size of the hash table:
@@ -152,13 +152,13 @@ auto rehash(struct bucket ** hashtableref, int64_t alloc_clusters) -> void
     - update variables
   */
 
-  auto * old_hashtable = *hashtableref;
+  auto * old_hashtable = hashtable_v.data();
   uint64_t const old_hashtablesize = 2 * alloc_clusters;
   uint64_t const new_hashtablesize = 2 * old_hashtablesize;
   uint64_t const new_hash_mask = new_hashtablesize - 1;
 
-  auto * new_hashtable =
-    (struct bucket *) xmalloc(sizeof(struct bucket) * new_hashtablesize);
+  std::vector<struct bucket> new_hashtable_v(new_hashtablesize);
+  auto * new_hashtable = new_hashtable_v.data();
   std::memset(new_hashtable, 0, sizeof(struct bucket) * new_hashtablesize);
 
   /* rehash all */
@@ -178,8 +178,7 @@ auto rehash(struct bucket ** hashtableref, int64_t alloc_clusters) -> void
         }
     }
 
-  xfree(old_hashtable);
-  *hashtableref = new_hashtable;
+  hashtable_v.swap(new_hashtable_v);
 }
 
 
@@ -327,8 +326,8 @@ auto derep(struct Parameters const & parameters, char * input_filename, bool use
 
   uint64_t hashtablesize = 2 * alloc_clusters;
   uint64_t hash_mask = hashtablesize - 1;
-  auto * hashtable =
-    (struct bucket *) xmalloc(sizeof(struct bucket) * hashtablesize);
+  std::vector<struct bucket> hashtable_v(hashtablesize);
+  auto * hashtable = hashtable_v.data();
   std::memset(hashtable, 0, sizeof(struct bucket) * hashtablesize);
 
   show_rusage();
@@ -426,7 +425,8 @@ auto derep(struct Parameters const & parameters, char * input_filename, bool use
         {
           uint64_t const new_alloc_clusters = 2 * alloc_clusters;
 
-          rehash(& hashtable, alloc_clusters);
+          rehash(hashtable_v, alloc_clusters);
+          hashtable = hashtable_v.data();
 
           alloc_clusters = new_alloc_clusters;
           hashtablesize = 2 * alloc_clusters;
@@ -964,8 +964,6 @@ auto derep(struct Parameters const & parameters, char * input_filename, bool use
     }
 
   show_rusage();
-
-  xfree(hashtable);
 
   show_rusage();
 }
