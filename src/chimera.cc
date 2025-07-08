7d067ae8dda27727d1b03a347f6130322b287eb6
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-08 14:46:40 +0200

    readability-identifier-length

--- src/chimera.cc
+++ src/chimera.cc
@@ -691,9 +691,9 @@ auto fill_max_alignment_length(struct chimera_info_s * chimera_info) -> void
 
   std::fill(chimera_info->maxi.begin(), chimera_info->maxi.end(), 0);
 
-  for (int f = 0; f < chimera_info->parents_found; ++f)
+  for (int i = 0; i < chimera_info->parents_found; ++i)
     {
-      int const best_parent = chimera_info->best_parents[f];
+      int const best_parent = chimera_info->best_parents[i];
       char * p = chimera_info->nwcigar[best_parent];
       char * e = p + std::strlen(p);
       int pos = 0;
