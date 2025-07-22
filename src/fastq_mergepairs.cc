327de1b1cadebbdba3805d4894cd13391d33dbf4
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-22 16:08:05 +0200

    eliminate direct call to malloc/free

--- src/fastq_mergepairs.cc
+++ src/fastq_mergepairs.cc
@@ -194,6 +194,7 @@ struct merge_data_s
   std::vector<char> fwd_header;
   std::vector<char> rev_header;
   std::vector<char> fwd_sequence;
+  std::vector<char> rev_sequence_v;
   char * rev_sequence = nullptr;
   char * fwd_quality = nullptr;
   char * rev_quality = nullptr;
@@ -1038,7 +1039,8 @@ auto read_pair(merge_data_t * a_read_pair) -> bool
         {
           a_read_pair->seq_alloc = seq_needed;
           a_read_pair->fwd_sequence.resize(seq_needed);
-          a_read_pair->rev_sequence = (char *) xrealloc(a_read_pair->rev_sequence, seq_needed);
+          a_read_pair->rev_sequence_v.resize(seq_needed);
+          a_read_pair->rev_sequence = a_read_pair->rev_sequence_v.data();
           a_read_pair->fwd_quality  = (char *) xrealloc(a_read_pair->fwd_quality,  seq_needed);
           a_read_pair->rev_quality  = (char *) xrealloc(a_read_pair->rev_quality,  seq_needed);
         }
@@ -1104,7 +1106,7 @@ auto free_merge_data(merge_data_t & a_read_pair) -> void
 {
   if (a_read_pair.rev_sequence != nullptr)
     {
-      xfree(a_read_pair.rev_sequence);
+      a_read_pair.rev_sequence = nullptr;
     }
   if (a_read_pair.fwd_quality != nullptr)
     {
