08b8ad5ecdaeb808351690e99b3356aa5d098eaf
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-08-18 16:52:01 +0200

    almost always use auto

--- src/derep.cc
+++ src/derep.cc
@@ -462,7 +462,7 @@ auto derep(struct Parameters const & parameters, char * input_filename, bool con
 
       auto * seq = fastx_get_sequence(input_handle);
       auto * header = fastx_get_header(input_handle);
-      int64_t const headerlen = fastx_get_header_length(input_handle);
+      auto const headerlen = fastx_get_header_length(input_handle);
       auto * qual = fastx_get_quality(input_handle); // nullptr if FASTA
 
       /* normalize sequence: uppercase and replace U by T  */
