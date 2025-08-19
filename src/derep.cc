b273af7d85a84b4a0a7d318dba3658a7b876b126
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-08-19 10:36:22 +0200

    readability-static-definition-in-anonymous-namespace

--- src/derep.cc
+++ src/derep.cc
@@ -82,7 +82,7 @@ namespace {
   // if performance are bad, see Victor_Ciura's Cpp Talk "So You Think You Can Hash"
   // then make a CityHash hasher object and use it with std::unordered_map
   using Hash = decltype(&hash_cityhash64);
-  static Hash hash_function = hash_cityhash64;
+  Hash hash_function = hash_cityhash64;
 
 
   struct bucket
