9a5873e4fd96da1fb4d0e29ae76b93b7b290cbf9
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-03-02 11:06:13 +0100

    eliminate function overload

--- src/showalign.cc
+++ src/showalign.cc
@@ -147,34 +147,6 @@ namespace {
 
 
   auto get_alignment_row(Span<char> const seq_view, Span<char> const cigar_view,
-                         int const alignlen, Viewpoint const viewpoint) -> std::vector<char> {
-    std::vector<char> row(alignlen + 1);
-    auto cursor_src = size_t{0};
-    auto cursor_dest = size_t{0};
-
-    auto const viewpoint_insertion = adapt_to_viewpoint(viewpoint);
-    for (auto const & a_pair: parse_cigar_string(cigar_view)) {
-      auto const operation = a_pair.first;
-      auto const runlength = a_pair.second;
-      assert(static_cast<size_t>(runlength) < row.size() - cursor_dest);
-      if ((operation == Operation::match) or
-          (operation == viewpoint_insertion)) {
-        auto const subsequence = seq_view.subspan(cursor_src, runlength);
-        std::copy(subsequence.cbegin(), subsequence.cend(), &row[cursor_dest]);
-        cursor_src += runlength;
-      } else {
-        // viewpoint_deletion = fill-in with gap symbols
-        std::fill_n(&row[cursor_dest], runlength, '-');
-      }
-      cursor_dest += runlength;
-    }
-
-    assert(row[cursor_dest] == '\0');
-    return row;
-  }
-
-
-  auto get_alignment_row(Span<char> const seq_view, Span<char> const cigar_view,
                          int const alignlen,
                          Operation const insertion_equivalent) -> std::vector<char> {
     std::vector<char> row(alignlen + 1);
